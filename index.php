7c576ab86daafe6895a80fb01f3d2f6485396eae
Girijajethitech <girijashankarmohanta11@gmail.com>
2023-12-02 08:16:37 +0530

    php full course

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>this is my php website</h1>
+
+
+    <?php
+        echo "hello";
+        $var1 = 10;
+        $var2 = 20;
+        echo $var1 + $var2;
+    ?>
+</body>
+</html>
